820d498be7ca5a22decf2083129a9400e9980a6c
ViniciusHenques <henquesvinicius@gmail.com>
2022-03-28 07:45:20 -0300

    Criacao do projeto

---
+++ pw3-hasfood-api/src/main/java/br/com/vinicius/henques/pw3hasfoodapi/Pw3HasfoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.vinicius.henques.pw3hasfoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pw3HasfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pw3HasfoodApiApplication.class, args);
+	}
+
+}
---
+++ pw3-hasfood-api/src/test/java/br/com/vinicius/henques/pw3hasfoodapi/Pw3HasfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vinicius.henques.pw3hasfoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw3HasfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
